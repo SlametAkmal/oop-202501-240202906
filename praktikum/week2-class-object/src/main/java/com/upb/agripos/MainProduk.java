57396f8849066ae132a334aad2f9601cc5fc5d9b
Slamet Akmal <kebumenkeren760@gmail.com>
2025-10-17 22:06:36 +0700

    week2-class-object.

---
+++ praktikum/week2-class-object/src/main/java/com/upb/agripos/MainProduk.java
@@ -0,0 +1,17 @@
+package com.upb.agripos;
+import com.upb.agripos.model.Produk;
+import com.upb.agripos.util.CreditBy;
+
+public class MainProduk {
+    public static void main(String[] args) {
+        Produk p1 = new Produk("SPK-001", "Speaker", 180000, 50);
+        Produk p2 = new Produk("KYB-101", "Keyboard", 140000, 20);
+        Produk p3 = new Produk("MUS-501", "Mouse", 95000, 15);
+
+        System.out.println("Kode: " + p1.getKode() + ", Nama: " + p1.getNama() + ", Harga: " + p1.getHarga() + ", Stok: " + p1.getStok());
+        System.out.println("Kode: " + p2.getKode() + ", Nama: " + p2.getNama() + ", Harga: " + p2.getHarga() + ", Stok: " + p2.getStok());
+        System.out.println("Kode: " + p3.getKode() + ", Nama: " + p3.getNama() + ", Harga: " + p3.getHarga() + ", Stok: " + p3.getStok());
+
+        CreditBy.print("240202906", "Slamet Akmal");
+    }
+}
---
+++ praktikum/week2-class-object/src/main/java/com/upb/agripos/model/Produk.java
@@ -0,0 +1,47 @@
+package com.upb.agripos.model;
+
+public class Produk {
+    private String kode;
+    private String nama;
+    private double harga;
+    private int stok;
+
+    public Produk(String kode, String nama, double harga, int stok) {
+        this.kode = kode;
+        this.nama = nama;
+        this.harga = harga;
+        this.stok = stok;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getKode() {
+        return kode;
+    }
+
+    public void setKode(String kode) {
+        this.kode = kode;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+
+    public void setHarga(double harga) {
+        this.harga = harga;
+    }
+
+    public int getStok() {
+        return stok;
+    }
+
+    public void setStok(int stok) {
+        this.stok = stok;
+    }
+}
