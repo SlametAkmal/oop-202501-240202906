c18aa428a69ea9fdaa7c4c4b55067763a31eb532
Slamet Akmal <kebumenkeren760@gmail.com>
2025-11-07 23:45:12 +0700

    praktikum/week3-inheritance/src/main/java/com/upb/agripos/MainInheritance.java

--- praktikum/week3-inheritance/src/main/java/com/upb/agripos/MainInheritance.java
+++ praktikum/week3-inheritance/src/main/java/com/upb/agripos/MainInheritance.java
@@ -8,7 +8,7 @@ import main.java.com.upb.agripos.util.CreditBy;
 public class MainInheritance {
     public static void main(String[] args) {
         Pupuk b = new Pupuk("BNH-001", "Banih Padi IR64", 25000,100, "IR64");
-        Benih p = new Benih("PPK-101", "Pupuk Urea", 350000, 40, "Komputer");
+        Benih p = new Benih("PPK-101", "Pupuk Urea", 350000, 40, "Urea");
         AlatPertanian a = new AlatPertanian("ALT-501", "Cangkul Baja", 90000, 15, "Baja");
 
         b.deskripsi();
